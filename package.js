c688a9e5f46df053d02f2537f8694f115a06b270
tworker <tworker@thoughtworks.com>
2015-06-25 10:51:30 -0700

    bump version again, looks like we can't delete broken packaages

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "CAS support for accounts",
-  version: "0.0.3",
+  version: "0.0.5",
   name: "twstudios:accounts-cas",
   git: "https://github.com/ThoughtWorksStudios/meteor-accounts-cas"
 });
